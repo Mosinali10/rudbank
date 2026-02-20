97fc3c40c6587811f882cf66697b2597d63ebc22
Mosin Ali <mosinaliaiml@gmail.com>
2026-02-20 15:49:37 +0530

    Create routing structure with auth and bank routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import bankRoutes from "./routes/bank.routes.js";
 
 dotenv.config();
 
@@ -11,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/bank", bankRoutes);
 
 const PORT = process.env.PORT || 5000;
 
